cf1b1aa41bf264cf80a479f9a1ee1e1989e2efe2
Julian Saenze <julian.saenze@comsysto.com>
2021-06-10 11:33:32 +0200

    building a custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -16,9 +16,20 @@ export class HomeComponent implements OnInit, OnDestroy {
     //taking a number as parameter (f.e. 1000) -> every second a new value will be fired
     //will continue to run, even if navigated away. Will start another when navigate back -> don't forget to cancel subscriptions
     //when they are no longer needed
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
-    })
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(()=> {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    });
   }
 
   ngOnDestroy(): void {
